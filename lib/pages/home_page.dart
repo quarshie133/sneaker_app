57001774acf1cbba4381262af452a2dd20474c4b
quarshie133 <wrightsprince@gmail.com>
2024-04-03 11:33:11 +0000

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:sneaker_app/components/bottom_nav_bar.dart';
+
+import 'cart_page.dart';
+import 'shop_page.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // this selected index is to control the botto nav bar
+  int _selectedIndex = 0;
+
+  // this method wil update our selected index
+  // when the user taps on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  //pages to display
+
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+
+    // cart page
+    const CartPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //logo
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Image.asset(
+                'lib/images/nike.png',
+                height: 240,
+              ),
+            ),
+
+            const SizedBox(
+              height: 40,
+            ),
+            // title
+            const Text(
+              'Just Do it',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+
+            // sub title
+            const Text(
+              'Brand new sneakers and custom kicks made with premium quality',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey,
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(
+              height: 40,
+            ),
+
+            // start now button
+
+            GestureDetector(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ),
+              ),
+              child: Container(
+                width: 335,
+                decoration: BoxDecoration(
+                    color: Colors.grey[900],
+                    borderRadius: BorderRadius.circular(12)),
+                padding: const EdgeInsets.all(25),
+                child: Center(
+                  child: const Text(
+                    'Shop Now',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
